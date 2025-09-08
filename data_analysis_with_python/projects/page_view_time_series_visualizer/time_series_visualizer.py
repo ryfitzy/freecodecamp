59f1dc56130dfe240a38457bb9a562c69ce47eca
Ryan Fitzgerald <sit28@txstate.edu>
2025-09-08 16:35:03 -0500

    completed line plot visualizer

--- data_analysis_with_python/projects/page_view_time_series_visualizer/time_series_visualizer.py
+++ data_analysis_with_python/projects/page_view_time_series_visualizer/time_series_visualizer.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
+import os
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv(f"{os.path.realpath(os.path.dirname(__file__))}/fcc-forum-pageviews.csv", index_col="date")
 
 # Clean data
-df = None
-
+df = df[(df["value"] >= df["value"].quantile(0.025)) & (df["value"] <= df["value"].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(16,5))
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
+    ax.plot(df.index.tolist(), df["value"], 'r')
 
-
-
-
+    ax.set_xticks(ticks=['2016-07-01', '2017-01-01', '2017-07-01', '2018-01-01', '2018-07-01', '2019-01-01', '2019-07-01', '2020-01-01'], labels=['2016-07', '2017-01', '2017-07', '2018-01', '2018-07', '2019-01', '2019-07', '2020-01'])
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -52,3 +55,5 @@ def draw_box_plot():
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
     return fig
+
+draw_line_plot()
