4a9d28549b5c839be565ca1ded1e45bc322c42db
Ryan Fitzgerald <sit28@txstate.edu>
2025-09-07 23:31:37 -0500

    completed medical-data-visualizer project

--- data_analysis_with_python/projects/medical_data_visualizer/medical_data_visualizer.py
+++ data_analysis_with_python/projects/medical_data_visualizer/medical_data_visualizer.py
@@ -2,34 +2,29 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # 1
-df = None
+df = pd.read_csv(f"{os.path.realpath(os.path.dirname(__file__))}/medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype('int8')
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype('int8')
+df['gluc'] = (df['gluc'] > 1).astype('int8')
 
 # 4
-def draw_cat_plot():
+def draw_cat_plot(savefig = False):
     # 5
-    df_cat = None
-
-
+    df_cat = pd.melt(df, id_vars="cardio", value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).size().reset_index(name="total")
 
     # 7
-
-
-
-    # 8
-    fig = None
-
-
+    g = sns.catplot(data=df_cat, x="variable", y="total", hue="value", col="cardio", kind="bar")
+    fig = g.figure
+    
     # 9
     fig.savefig('catplot.png')
     return fig
@@ -38,23 +33,23 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
-
+    corr = df_heat.corr()
+    
     # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 15
+    fig, ax = plt.subplots(figsize=(12, 12))
 
-
-
-    # 16
-    fig.savefig('heatmap.png')
+    # 15: Draw heatmap
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
     return fig
